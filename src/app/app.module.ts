dbcd0db09573b50b3a3c2697c6dfd322caa45243
LimbergDV <limbergdelvalle@gmail.com>
2025-03-19 22:17:09 -0600

    prueba de rama (eliminar despues)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp } from 'firebase/app';
 import { getMessaging } from 'firebase/messaging';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NavbarComponent } from './ui/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
--- src/app/ui/shared/navbar/navbar.component.ts
+++ src/app/ui/shared/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isOpen = false;
 
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
